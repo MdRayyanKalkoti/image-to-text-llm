a4c9bdd23fa0e7f14384ba9687b3bdf01f886192
Md Rayyan <rayyanaiengineer@gmail.com>
2026-04-09 20:30:10 +0530

    fix: patch easyocr bidi import on Render

---
+++ postinstall.py
@@ -0,0 +1,9 @@
+﻿import site, os
+for sp in site.getsitepackages():
+    f = os.path.join(sp, 'easyocr', 'easyocr.py')
+    if os.path.exists(f):
+        c = open(f).read()
+        if 'from bidi import get_display' in c:
+            open(f,'w').write(c.replace('from bidi import get_display','from bidi.algorithm import get_display'))
+            print('Patched easyocr bidi import')
+        break
